47cf0221cbcf35c95c919b898f52439863d901e7
rhems234 <rhems234@gmail.com>
2025-11-14 19:44:11 +0900

    (2025.11.14) 선택 정렬

---
+++ Program/Class 5th ( Insert Sort)/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘 입니다.
+
+	int arr[] = { 5, 2, 3, 6, 9, 7, 8 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int key, j;
+
+	for (int i = 0; i < size; i++)
+	{
+		key = arr[i];
+		for (j = i - 1; j >= 0 && arr[j] > key; j--)
+		{
+			if (arr[j] > key)
+			{
+				arr[j + 1] = arr[j];
+			}
+			else
+			{
+				break;
+			}
+		}
+		arr[j + 1] = key;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -5,7 +5,32 @@ using namespace std;
 int main()
 {
 
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
 
+	int x = 24;
+	int y = 36;
+	int z = 0;
+
+	/*while (y != 0)
+	{
+		z = x % y;
+		x = y;
+		y = z;
+	}*/
+
+	for (int i = 1; i <= x && i <= y; i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			z = i;
+		}
+	}
+
+	cout << "Greatest Common Divisor :  : " << x;
+
+#pragma endregion
 
 	return 0;
 }
