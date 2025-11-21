6d4d00c90248e85c31ae1a5e71467b577c246827
rhems234 <rhems234@gmail.com>
2025-11-21 19:46:00 +0900

    (2025.11.21) 퀵 정렬

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,10 +2,86 @@
 
 using namespace std;
 
+void combine(int list[], int start, int middle, int end)
+{
+	int* newcontainer = new int[end - start + 1];
+	int count = 0;
+
+	int left = start;
+	int right = middle + 1;
+
+	while (left <= middle && right <= end)
+	{
+		if (list[left] <= list[right])
+		{
+			newcontainer[count] = list[left++];
+		}
+		else
+		{
+			newcontainer[count] = list[right++];
+		}
+	}
+
+	while (left <= middle)
+	{
+		newcontainer[count++] = list[left++];
+	}
+
+	while (right <= end)
+	{
+		newcontainer[count++] = list[right++];
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		list[start + i] = newcontainer[i];
+
+	}
+
+
+	delete[] newcontainer;
+
+}
+
+void merge_sort(int list[], int start, int end)
+{
+
+	if (start < end)
+	{
+		int middle= (start + end) / 2;
+
+		merge_sort(list, start, middle);
+
+		merge_sort(list, middle + 1, end);
+
+		combine(list, start, middle, end);
+	}
+
+}
+
 int main()
 {
-	
+#pragma region 병합 정렬
+	// 하나의 리스트를 두 개의 균일한 크기로 분할하고 분할된
+	// 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
+	// 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
 
+	// 1. 리스트의 길이가 0 또는 1이 되면 이미 정렬된 것으로 봅니다.
+
+	// 2. 그렇지 않은 경우
+	// 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의
+	//		두 부분 리스트로 나눕니다.
+
+	// 2-2. 각 부분 리스트를 재귀적으로 합병 정렬을 이용하여 정렬합니다.
+
+	// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
+
+	int list[] = { 3, 5, 2, 7, 4, 1, 8, 6 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	merge_sort(list, 0, size - 1);
+
+#pragma endregion
 
 	return 0;
 }
