3747401799e4b6d41c2637e4aacfb22c7b6266c4
rhems234 <rhems234@gmail.com>
2025-11-24 19:46:33 +0900

    (2025.11.24) 병합 정렬

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,86 +2,61 @@
 
 using namespace std;
 
-void combine(int list[], int start, int middle, int end)
+int Fibonacci(int n, int list[])
 {
-	int* newcontainer = new int[end - start + 1];
-	int count = 0;
-
-	int left = start;
-	int right = middle + 1;
-
-	while (left <= middle && right <= end)
+	// 피보나치 재귀 함수
+	/*if (n <= 1)
 	{
-		if (list[left] <= list[right])
-		{
-			newcontainer[count] = list[left++];
-		}
-		else
-		{
-			newcontainer[count] = list[right++];
-		}
+		return n;
 	}
 
-	while (left <= middle)
+	return Fibonacci(n - 1) + Fibonacci(n - 2);*/
+	////
+
+	if (n <= 1)
 	{
-		newcontainer[count++] = list[left++];
+		list[n] = n;
+		return list[n];
 	}
 
-	while (right <= end)
-	{
-		newcontainer[count++] = list[right++];
-	}
-
-	for (int i = 0; i < count; i++)
-	{
-		list[start + i] = newcontainer[i];
-
-	}
-
-
-	delete[] newcontainer;
-
-}
-
-void merge_sort(int list[], int start, int end)
-{
-
-	if (start < end)
-	{
-		int middle= (start + end) / 2;
-
-		merge_sort(list, start, middle);
-
-		merge_sort(list, middle + 1, end);
-
-		combine(list, start, middle, end);
-	}
+	list[n] = Fibonacci(n - 1, list) + Fibonacci(n - 2, list);
+	return list[n];
 
 }
 
 int main()
 {
-#pragma region 병합 정렬
-	// 하나의 리스트를 두 개의 균일한 크기로 분할하고 분할된
-	// 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
-	// 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
+#pragma region 동적 계획법
+	// 하나의 큰 문제를 여러 개의 작은 문제로 나누어서 그 결과를
+	// 저장하여 다시 큰 문제를 해결할 때 사용하는 알고리즘 입니다.
 
-	// 1. 리스트의 길이가 0 또는 1이 되면 이미 정렬된 것으로 봅니다.
+	// 겹치는 부분 문제 ( Overlapping Subproblems )
+	// 동일한 작은 문제들이 반복하여 나타나는 경우를 의미합니다.
 
-	// 2. 그렇지 않은 경우
-	// 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의
-	//		두 부분 리스트로 나눕니다.
+	// 최적 부분 구조 ( Optimal Substructure )
+	// 부분 문제의 최적 결과 값을 사용하여 전체 문제의 최적의
+	// 결과를 낼 수 있는 경우를 의미합니다.
 
-	// 2-2. 각 부분 리스트를 재귀적으로 합병 정렬을 이용하여 정렬합니다.
-
-	// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
-
-	int list[] = { 3, 5, 2, 7, 4, 1, 8, 6 };
-	int size = sizeof(list) / sizeof(list[0]);
-
-	merge_sort(list, 0, size - 1);
+	// 메모이제이션 ( Memoization )
+	// 프로그램이 동일한 계산을 반복해야할 때, 이전에 계산한 값을
+	// 메모리에 저장함으로써 동일한 계산을 반복 수행하는 작업을
+	// 제거하여 프로그램의 실행 속도를 향상시키는 방법 입니다.
 
 #pragma endregion
+
+	// 피보나치 재귀 함수 출력
+	/*int n = 3;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << Fibonacci(i) << " ";
+	}*/
+
+	int n = 45;
+	int* arr = new int[n + 1];
+
+	cout << Fibonacci(n, arr) << endl;
+	
 
 	return 0;
 }
